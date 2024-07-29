aa88096c4ae4b4112c508699d10b0d3f98a650ed
abrahm <wole1702@gmail.com>
2024-07-29 13:20:44 +0100

    code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""A script that starts a flask web application
-Your web application must be listening on 0.0.0.0, port 5000
-"""
+""" script that starts a flash application """
 
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """Return a given string"""
-    return ("Hello HBNB!")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "<p>Hello HBNB!</p>"
