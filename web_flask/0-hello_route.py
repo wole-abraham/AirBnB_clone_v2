94923eadffbe4faba1a24a0a01c3424efb45e7da
abrahm <wole1702@gmail.com>
2024-07-29 13:25:56 +0100

    coe

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_world():
     return "<p>Hello HBNB!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
